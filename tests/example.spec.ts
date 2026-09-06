8dca8f031202a3203b18600aaa2bee63a1f3aca7
steveruffin5076 <stevechong007@yahoo.com>
2026-09-06 16:01:27 +0800

    Add second test: check a checkbox

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,3 +4,11 @@ test('homepage has correct title', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
   await expect(page).toHaveTitle('The Internet');
 });
+
+
+test('checkbox can be checked', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/checkboxes');
+  const firstCheckbox = page.locator('input[type="checkbox"]').first();
+  await firstCheckbox.check();
+  await expect(firstCheckbox).toBeChecked();
+});
